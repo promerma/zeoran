9f99d05094105f8fc0f9620398de1ae13e9b26fa
Pablo Romero Marimon <99318107+promerma@users.noreply.github.com>
2023-02-11 16:11:14 +0100

    Add files via upload

---
+++ libraries.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <cstring>
+#include <iomanip>
+#include <string> 
+#include <Eigen/Eigenvalues>
+using namespace std;
