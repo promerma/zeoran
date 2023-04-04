6c712a68240589a9853d687bc43d41d37cb24ec0
Pablo Romero Marimon <99318107+promerma@users.noreply.github.com>
2023-04-04 11:28:12 +0200

    Update new global.h for version2

---
+++ global.h
@@ -0,0 +1,15 @@
+//Unit cell parameters
+int Natoms, Tatoms;
+double a, b, c;
+double alpha, beta, gama;
+string setting;
+
+//Default merw parameters
+int Neqsteps=100;	//Number of steps to find equilibrium position
+int Nvisits=20;		//Number of visits needed to select a new Al
+
+//atom structure
+typedef struct{
+  char id[5], at[5];
+  double x, y, z, q;
+} atom;
